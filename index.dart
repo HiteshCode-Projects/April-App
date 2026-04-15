e3ce5a05f6f449e1a25818397be842edbffad795
HiteshCode-Projects <hiteshtokasiya@gmail.com>
2026-04-15 21:36:31 +0530

    variable

---
+++ index.dart
@@ -0,0 +1,18 @@
+void main() {
+  print("Hello guys How are you doing?");
+
+  print("I am fine, thank you for asking.");
+
+  print("What about you?");
+
+  //String Means "Anything in double quotes is a string"  or 'Anything in single quotes is a string' - Textual data
+
+  String username = "Krisha";
+  print(username);
+  String password = "12345";
+  print(password);
+
+  //Integer means Any whole number without a decimal point - Numeric data
+  int age = 25;
+  print(age);
+}
